355a6319eb81f8910d03bead592e7517aba1dc37
Rommel Terrence Juanillo <terrence@newlogic.io>
2019-11-07 11:33:32 +0800

    add status to exceptions

--- openabis_ageitgey_face_recognition/exceptions.py
+++ openabis_ageitgey_face_recognition/exceptions.py
@@ -1,6 +1,12 @@
 class FaceNotRecognized(Exception):
     """Exception for not recognized face in the image."""
 
+    def __init__(self):
+        self.status = 406
+
 
 class MultipleFacesDetected(Exception):
     """Exception for multiple faces in the image."""
+
+    def __init__(self):
+        self.status = 406
