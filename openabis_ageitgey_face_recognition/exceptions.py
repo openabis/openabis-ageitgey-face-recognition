2f04a27df85499695477786e10a125820b59241b
Rommel Terrence Juanillo <terrence@newlogic.io>
2019-11-07 13:10:28 +0800

    error 406 to 400

--- openabis_ageitgey_face_recognition/exceptions.py
+++ openabis_ageitgey_face_recognition/exceptions.py
@@ -1,12 +1,14 @@
 class FaceNotRecognized(Exception):
     """Exception for not recognized face in the image."""
 
-    def __init__(self):
-        self.status = 406
+    def __init__(self, *args):
+        self.status = 400
+        super(FaceNotRecognized, self).__init__(*args)
 
 
 class MultipleFacesDetected(Exception):
     """Exception for multiple faces in the image."""
 
-    def __init__(self):
-        self.status = 406
+    def __init__(self, *args):
+        self.status = 400
+        super(MultipleFacesDetected, self).__init__(*args)
